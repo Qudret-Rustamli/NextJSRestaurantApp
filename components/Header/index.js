a0c8e74bf0c92cd9b664c8baa7c7bd373c190c9f
thehasanovv <hasanov.e95@gmail.com>
2022-06-13 23:39:08 +0400

    Sass Architecture Structure

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import Links from './Links';
-import Logo from './Logo';
+import React from "react";
+import Links from "./Links";
+import Logo from "./Logo";
+import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
-    <>
+    <div className={styles.container}>
       <Logo />
       <Links />
-    </>
+    </div>
   );
 };
 
