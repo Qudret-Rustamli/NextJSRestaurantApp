c5d29b466313b5ea13ab3137ba7ac395d56d1b92
Qudret-Rustamli <qudret.rustem@gmail.com>
2022-06-18 01:36:59 +0400

    homepage part-1

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,11 +1,21 @@
-import styles from "./Button.module.scss";
+import styles from './Button.module.scss';
 
-const Button = ({ width, children }) => {
+const Button = ({ width, children, borderRadius, color, height, backgroud,border }) => {
   return (
-    <button className={styles.button} style={{ width: `${width}px` || `100%` }}>
+    <button
+      className={styles.button}
+      style={{
+        width: width ? width : '100px',
+        height: height ? height : '40px',
+        borderRadius: borderRadius ? borderRadius : '0px',
+        color: color ? color : '#fff',
+        backgroundColor: backgroud ? backgroud : 'transparent',
+        border: border ? border : 'none'
+      }}>
       {children}
     </button>
   );
 };
 
 export default Button;
+//#d63626
--- features/Home.js
+++ features/Home.js
@@ -1,60 +1,143 @@
+import Button from '../components/Button';
+import BigCard from '../components/HomePageContents/Card/BigCard';
+import BigPanel from '../components/HomePageContents/Panel/BigPanel';
+import SmallPanel from '../components/HomePageContents/Panel/SmallPanel';
 import style from './style.module.scss';
 
-const Home = () => {
+//icons
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitterSquare,
+  FaYoutube,
+  FaLinkedin,
+  FaTiktok,
+} from 'react-icons/fa';
+import {MdOutgoingMail,MdLocationPin} from 'react-icons/md';
+
+const HomeFeature = () => {
   return (
     <div className={style.container}>
       {/* ====================================================================== */}
       <div className={style.container__mainContent}>
         <div className={style.container__mainContent__subLeft}>
-          <div className={style.container__mainContent__subLeft__title}></div>
-          <div className={style.container__mainContent__subLeft__content}></div>
+          <div className={style.container__mainContent__subLeft__title}>
+            Our Food Site Makes It Easy To Find Local Food
+          </div>
+          <div className={style.container__mainContent__subLeft__content}>
+            Lorem Ipsum Is Placeholder Text Commonly Used In The Graphic, Print, And Publishing
+            Industries For Previewing Layouts And Visual Mockups.
+          </div>
           <div className={style.container__mainContent__subLeft__btns}>
-            <button>First</button>
-            <button>Second</button>
+            <Button width="35%" height="5rem" borderRadius="2rem" color="white" backgroud="#d63626">
+              {' '}
+              Sign Up
+            </Button>
+            <Button
+              width="35%"
+              height="5rem"
+              borderRadius="2rem"
+              color="black"
+              border="1px solid black">
+              Order Now
+            </Button>
           </div>
         </div>
-        <div className={style.container__mainContent__subRight}></div>
+        <div className={style.container__mainContent__subRight}>
+          <BigPanel />
+        </div>
       </div>
       {/* ====================================================================== */}
       <div className={style.container__featureContent}>
-        <div className={style.container__featureContent__title}></div>
-        <div className={style.container__featureContent__content}></div>
-        <div className={style.container__featureContent__cards}></div>
+        <div className={style.container__featureContent__title}>Features</div>
+        <div className={style.container__featureContent__content}>
+          Lorem Ipsum Is Placeholder Commonly Used In The Graphic
+        </div>
+        <div className={style.container__featureContent__cards}>
+          <BigCard />
+          <BigCard />
+          <BigCard />
+        </div>
       </div>
       {/* ====================================================================== */}
       <div className={style.container__Content__first}>
         <div className={style.container__Content__first__left}>
-          <div className={style.container__Content__first__left__title}></div>
-          <div className={style.container__Content__first__left__content}></div>
+          <h1 className={style.container__Content__first__left__title}>
+            Menu That Always Make You Fall In Love
+          </h1>
+          <div className={style.container__Content__first__left__content}>
+            Lorem Ipsum Is Placeholder Text Commonly Used In The Graphic, Print, And Publishing
+            Industries For Previewing Layouts And Visual Mockups.Lorem Ipsum Is Placeholder Text
+            Commonly Used In The Graphic, Print, And Publishing Industries For Previewing Layouts
+            And Visual Mockups.
+          </div>
         </div>
-        <div className={style.container__Content__first__right}>Cards</div>
+        <div className={style.container__Content__first__right}>
+          <SmallPanel />
+        </div>
       </div>
       {/* ====================================================================== */}
       <div className={style.container__Content__second}>
-        <div className={style.container__Content__second__left}>cards</div>
+        <div className={style.container__Content__second__left}>
+          <SmallPanel />
+        </div>
         <div className={style.container__Content__second__right}>
-          <div className={style.container__Content__second__right__title}></div>
-          <div className={style.container__Content__second__right__content}></div>
+          <div className={style.container__Content__second__right__title}>
+            Yummy Always Papa John’s Pizza.Agree?
+          </div>
+          <div className={style.container__Content__second__right__content}>
+            Lorem Ipsum Is Placeholder Text Commonly Used In The Graphic, Print, And Publishing
+            Industries For Previewing Layouts And Visual Mockups.Lorem Ipsum Is Placeholder Text
+            Commonly Used In The Graphic, Print, And Publishing Industries For Previewing Layouts
+            And Visual Mockups.
+          </div>
         </div>
       </div>
       {/* ====================================================================== */}
       <div className={style.container__Content__third}>
         <div className={style.container__Content__third__left}>
-          <div className={style.container__Content__third__left__title}></div>
-          <div className={style.container__Content__third__left__content}></div>
+          <div className={style.container__Content__third__left__title}>
+            Do You Like French Fries? Mmm...
+          </div>
+          <div className={style.container__Content__third__left__content}>
+            Lorem Ipsum Is Placeholder Text Commonly Used In The Graphic, Print, And Publishing
+            Industries For Previewing Layouts And Visual Mockups.Lorem Ipsum Is Placeholder Text
+            Commonly Used In The Graphic, Print, And Publishing Industries For Previewing Layouts
+            And Visual Mockups.
+          </div>
         </div>
-        <div className={style.container__Content__third__right}></div>
+        <div className={style.container__Content__third__right}>
+          <SmallPanel />
+        </div>
       </div>
       {/* ====================================================================== */}
       <div className={style.container__newFoods}>
-        <div className={style.container__newFoods__up}>
-            <div className={style.container__newFoods__up__title}></div>
-            <div className={style.container__newFoods__up__content}></div>
+        <div className={style.container__newFoods__title}>Our Popular Update New Foods</div>
+        <div className={style.container__newFoods__content}>
+          Lorem Ipsum Is Placeholder Commonly Used In The Graphic
         </div>
-        <div className={style.container__newFoods__down}>Cards</div>
+        <div className={style.container__newFoods__down}>
+          <BigCard />
+          <BigCard />
+          <BigCard />
+        </div>
+      </div>
+      {/* ====================================================================== */}
+      <div className={style.container__footer}>
+        <h1 className={style.container__footer__title}>Our Sosial Media Accounts</h1>
+        <div className={style.container__footer__icons}>
+          <FaFacebook />
+          <FaInstagram />
+          <FaTwitterSquare />
+          <FaYoutube />
+          <FaLinkedin />
+          <FaTiktok />
+          <MdOutgoingMail/>
+          <MdLocationPin/>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Home;
+export default HomeFeature;
